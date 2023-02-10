bff55bbda9065872f1e830d4176a6309f6a05301
Phenixis22 <max.duh22@gmail.com>
2023-02-10 10:04:26 +0100

    update persos, pokemon, constants

--- Persos.py
+++ Persos.py
@@ -15,11 +15,3 @@ class Perso:
 
 def dis(text):
     print(text)
-
-
-Red = Perso("Red", ["pkmn1"], "dresseur")
-Red.def_interaction("Je te défie !")
-Red.interaction()
-
-Red.def_interaction("Tu es encore là toi !")
-Red.interaction()
--- constants.py
+++ constants.py
@@ -1,10 +1,12 @@
+from random import randint
 import pyxel
 from Capacite import *
 from Pokemon import *
 from Persos import *
-from random import randint
 
 TYPES = {'physique': ["Acier", "Combat", "Insecte", "Normal", "Poison", "Roche", "Sol", "Spectre", "Vol"],
          'special': ["Dragon", "Eau", "Electrik", "Feu", "Glace", "Plante", "Psy", "Ténèbres"]}
+
 STATUTS = ["eveille","brûlure", "gel", "paralysie", "empoisement", "sommeil", "confus"]
-CAP = [...]
+
+CAP = {"charge": Capacite("physique", "normal", 35, 40, 1)}
