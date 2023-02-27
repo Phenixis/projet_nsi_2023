74d5a1ecf49df557d224282001da296f342cfd13
Phenixis22 <max.duh22@gmail.com>
2023-02-27 14:29:04 +0100

    ON ARTRETE VRAIMENT POKEMON ! RAGEQUIT

--- Trash/Capacite.py
+++
@@ -1,9 +0,0 @@
-class Capacite:
-
-    def __init__(self, categorie: str, type: str, PP: int, puissance:int, precision: int):
-        self.categ = categorie
-        self.type = type
-        self.PP = PP
-        self.puissance = puissance
-        self.precision = precision
-
