c893d3b4d8f505094c97c1f432583380657dd8b0
Phenixis22 <max.duh22@gmail.com>
2023-03-09 16:46:34 +0100

    Déplacement de la classe Joueur dans fichier unique

---
+++ Joueur.py
@@ -0,0 +1,41 @@
+from constants import *
+
+class Joueur:
+    """
+    Crée un personnage
+    """
+    def __init__(self):
+        self.width = 30
+        self.height = 42
+
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.fill(RED)
+
+        self.column = 0
+        self.row = 0
+        self.coor = [0.0, 0.0]
+        self.define_coor()
+
+    def update(self, dir: str):
+        """
+        Permet un mouvement
+        "r" correspond à "right"
+        "l" correspond à "left"
+        "u" correspond à "up"
+        "d" correspond à "down"
+        """
+        if dir == "r":
+            self.column += 1
+        if dir == "l":
+            self.column -= 1
+        if dir == "u":
+            self.row -= 1
+        if dir == "d":
+            self.row += 1
+        self.define_coor()
+
+    def define_coor(self):
+        """
+        Défini les coordonnées x et y du joueur
+        """
+        self.coor = [self.column * 50 + 10, self.row * 50 + 4]
--- Labyrinthe.py
+++ Labyrinthe.py
@@ -1,48 +1,7 @@
 from constants import *
 import networkx as nx
 from random import choice
-import pygame
-
-class Joueur:
-    """
-    Crée un personnage
-    """
-    def __init__(self):
-        self.width = 30
-        self.height = 42
-
-        self.image = pygame.Surface([self.width, self.height])
-        self.image.fill(RED)
-
-        self.column = 0
-        self.row = 0
-        self.coor = [0.0, 0.0]
-        self.define_coor()
-
-    def update(self, dir: str):
-        """
-        Permet un mouvement
-        "r" correspond à "right"
-        "l" correspond à "left"
-        "u" correspond à "up"
-        "d" correspond à "down"
-        """
-        if dir == "r":
-            self.column += 1
-        if dir == "l":
-            self.column -= 1
-        if dir == "u":
-            self.row -= 1
-        if dir == "d":
-            self.row += 1
-        self.define_coor()
-
-    def define_coor(self):
-        """
-        Défini les coordonnées x et y du joueur
-        """
-        self.coor = [self.column * 50 + 10, self.row * 50 + 4]
-
+from Joueur import *
 
 def neighbors(x: int, y: int) -> list:
     """
