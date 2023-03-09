80fd8e2c99c3b6816448faa1f9825603f4c4a018
Phenixis22 <max.duh22@gmail.com>
2023-03-09 16:43:27 +0100

    Update Labyrinthe
    Déplacement de labyrinthe
    Annotation ajoutée au fonctions pour les comprendre facilement

--- Labyrinthe.py
+++ Labyrinthe.py
@@ -1,24 +1,12 @@
+from constants import *
 import networkx as nx
 from random import choice
 import pygame
 
-HEIGHT = 1450
-WIDTH = 840
-COLUMNS = 24  # nombre de colonnes
-ROWS = 16  # nombre de lignes
-SIZE = 50  # taille d'une case
-WHITE = (255, 255, 255)
-GREY = (20, 20, 20)
-BLACK = (0, 0, 0)
-PURPLE = (100, 0, 100)
-RED = (255, 0, 0)
-
-pygame.init()
-clock = pygame.time.Clock()
-
-
 class Joueur:
-
+    """
+    Crée un personnage
+    """
     def __init__(self):
         self.width = 30
         self.height = 42
@@ -26,27 +14,37 @@ class Joueur:
         self.image = pygame.Surface([self.width, self.height])
         self.image.fill(RED)
 
-        self.x = 0
-        self.y = 0
+        self.column = 0
+        self.row = 0
         self.coor = [0.0, 0.0]
         self.define_coor()
 
     def update(self, dir: str):
+        """
+        Permet un mouvement
+        "r" correspond à "right"
+        "l" correspond à "left"
+        "u" correspond à "up"
+        "d" correspond à "down"
+        """
         if dir == "r":
-            self.x += 1
+            self.column += 1
         if dir == "l":
-            self.x -= 1
+            self.column -= 1
         if dir == "u":
-            self.y -= 1
+            self.row -= 1
         if dir == "d":
-            self.y += 1
+            self.row += 1
         self.define_coor()
 
     def define_coor(self):
-        self.coor = [self.x * 50 + 10, self.y * 50 + 4]
+        """
+        Défini les coordonnées x et y du joueur
+        """
+        self.coor = [self.column * 50 + 10, self.row * 50 + 4]
 
 
-def neighbors(x: int, y: int) -> list[tuple[int, int]]:
+def neighbors(x: int, y: int) -> list:
     """
     Renvoie la liste des cases 'voisines' de (x,y) par leur position, pas nécessairement connectées
     """
@@ -58,7 +56,7 @@ def neighbors(x: int, y: int) -> list[tuple[int, int]]:
                                                                          if dy != 0]
 
 
-def propagate(x, y, new) -> None:
+def propagate(region, x, y, new) -> None:
     """
     utilise un parcours en profondeur pour propager une valeur dans toutes les cellules d'une même région
     """
@@ -72,27 +70,31 @@ def propagate(x, y, new) -> None:
                 stack.append(n)
 
 
-def draw_lab(surface):
-    pygame.draw.rect(surface, (255, 255, 255), pygame.Rect(0, 0, COLUMNS * 50, ROWS * 50))
-
-    pygame.draw.line(surface, (0, 0, 0), (0, 0), (COLUMNS * 50, 0), 5)
-    pygame.draw.line(surface, (0, 0, 0), (COLUMNS * 50, 0), (COLUMNS * 50, ROWS * 50), 5)
-    pygame.draw.line(surface, (0, 0, 0), (COLUMNS * 50, ROWS * 50), (0, ROWS * 50), 5)
-    pygame.draw.line(surface, (0, 0, 0), (0, ROWS * 50), (0, 0), 5)
+def draw_lab(surface, g):
+    """
+    Dessine le labyrinthe
+    """
+    pygame.draw.rect(surface, (255, 255, 255), pygame.Rect(0, 0, COLUMNS * 50, ROWS * 50)) # background
+    pygame.draw.line(surface, (0, 0, 0), (0, 0), (COLUMNS * 50, 0), 5) # ligne du haut
+    pygame.draw.line(surface, (0, 0, 0), (COLUMNS * 50, 0), (COLUMNS * 50, ROWS * 50), 5) # ligne de droite
+    pygame.draw.line(surface, (0, 0, 0), (COLUMNS * 50, ROWS * 50), (0, ROWS * 50), 5) # ligne du bas
+    pygame.draw.line(surface, (0, 0, 0), (0, ROWS * 50), (0, 0), 5) # ligne de gauche
 
     for x in range(COLUMNS):
         for y in range(ROWS):
             if (x + 1, y) not in g.neighbors((x, y)):  # mur vertical
                 pygame.draw.line(surface, (0, 0, 0), ((x + 1) * SIZE, y * SIZE), ((x + 1) * SIZE, (y + 1) * SIZE), 3)
-
             if (x, y + 1) not in g.neighbors((x, y)):  # mur horizontal
                 pygame.draw.line(surface, (0, 0, 0), (x * SIZE, (y + 1) * SIZE), ((x + 1) * SIZE, (y + 1) * SIZE), 3)
 
 
 def fill_region():
+    """
+    Rempli un dictionnaire nommé "region" des cases du labyrinthe avec une valeur de plus en plus grande
+    Utile pour créer le graphe plus tard
+    """
     region = {}
     i = 0
-
     for y in range(ROWS):
         for x in range(COLUMNS):
             region[(x, y)] = i
@@ -101,7 +103,10 @@ def fill_region():
     return region
 
 
-def define_graph():
+def define_graph(region):
+    """
+    Défini le graphe du labyrinthe permettant de dessigner les murs ensuite
+    """
     nb_cases = COLUMNS * ROWS
 
     g = nx.Graph()
@@ -112,7 +117,7 @@ def define_graph():
         x2, y2 = choice(neighbors(x1, y1))
         if region[x1, y1] != region[x2, y2]:
             g.add_edge((x1, y1), (x2, y2))
-            propagate(x1, y1, region[x2, y2])
+            propagate(region, x1, y1, region[x2, y2])
             nb_cases -= 1
 
     positions = {(x, y): (0.5 * x, 0.5 * (ROWS - y)) for (x, y) in region.keys()}
@@ -122,32 +127,12 @@ def define_graph():
     return g
 
 
-def main(screen):
-    region = fill_region()
-    g = define_graph()
-    draw_lab(screen)
-
-
-# joueur = Joueur()
-#
-# go_on = True
-# while go_on:
-#     pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(joueur.coor[0], joueur.coor[1], joueur.width, joueur.height))
-#     for event in pygame.event.get():
-#         if "key" in event.dict and event.dict['key'] == 27:
-#             go_on = False
-#         if "text" in event.dict.keys():
-#             if event.dict['text'] == 'q' and ((joueur.x, joueur.y), (joueur.x - 1, joueur.y)) in g.edges:
-#                 joueur.update("l")
-#             elif event.dict['text'] == 'd' and ((joueur.x, joueur.y), (joueur.x + 1, joueur.y)) in g.edges:
-#                 joueur.update("r")
-#             elif event.dict['text'] == 's' and ((joueur.x, joueur.y), (joueur.x, joueur.y + 1)) in g.edges:
-#                 joueur.update("d")
-#             elif event.dict['text'] == 'z' and ((joueur.x, joueur.y), (joueur.x, joueur.y - 1)) in g.edges:
-#                 joueur.update("u")
-#
-#     screen.blit(joueur.image, joueur.coor)
-#     pygame.display.update()
-#     clock.tick(60)
-#
-# pygame.quit()
+def main(surface):
+    """
+    Rempli region, defini le graphe selon region
+    Dessine le labyrinthe
+    Renvoie le graphe
+    """
+    graphe = define_graph(fill_region())
+    draw_lab(surface, graphe)
+    return graphe
