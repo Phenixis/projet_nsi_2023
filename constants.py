bad17006df225f8c7382f3ef310f272f2f47e15a
Phenixis22 <max.duh22@gmail.com>
2023-03-09 16:42:40 +0100

    Update constants
    J'ai fait en sortes que le labyrinthe soit de la taille la plus grande possible tout en laissant 200 pixels minimum à droite et 100 pixels en bas

--- constants.py
+++ constants.py
@@ -1,8 +1,13 @@
-HEIGHT = 1450
-WIDTH = 840
-COLUMNS = 24  # nombre de colonnes
-ROWS = 16  # nombre de lignes
+from win32api import GetSystemMetrics
+
+# Taille utiles
+WIDTH = GetSystemMetrics(0)
+HEIGHT = GetSystemMetrics(1)
 SIZE = 50  # taille d'une case
+COLUMNS = (WIDTH//SIZE)-4 # nombre de colonnes
+ROWS = (HEIGHT//SIZE)-2 # nombre de lignes
+
+# Couleur
 WHITE = (255, 255, 255)
 GREY = (20, 20, 20)
 BLACK = (0, 0, 0)
